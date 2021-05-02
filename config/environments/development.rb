e97408e0f0ae2fca6057c86f5cc1cc80f8c2b41d
Ubuntu <ubuntu@ip-172-31-38-133.ap-northeast-1.compute.internal>
2021-05-02 08:54:17 +0000

    routesの変更・devise初期設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  host = '9aadcdb2d2dc4a849f5dc917d01c36bc.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host , protocol: 'https' }
 end
